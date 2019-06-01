72cb482acf6d439b05aa85f85816fbbaf1a05e76
Dennis <51213179+dheijmans@users.noreply.github.com>
2019-06-01 11:34:35 +0200

    Dennis: Levels.cs shorter and added Game Loop

--- Guessing game/Program.cs
+++ Guessing game/Program.cs
@@ -5,43 +5,56 @@ namespace Guessing_game
     class Program
     {
         static void Main(string[] args)
-        {           
-            int levelChoice;
-            int numberRange = 0;
-            bool hasChosen;
-            Frame.Title();           
-            Console.WriteLine("Welcome to the random number guessing game");            
-            do {                
-                hasChosen = true;
-                Console.WriteLine("Easy - 0");
-                Console.WriteLine("Medium - 1");
-                Console.WriteLine("Hard - 2");
-                Console.Write("What level do you want to play? ");
-            try {
-                levelChoice = Convert.ToInt32(Console.ReadLine());
+        {
+            do
+            {
+                int levelChoice;
+                int numberRange = 0;
+                bool invalidInput;
+                string playAgainInput;
+                Console.Clear();
+                Frame.Title();
+                Console.WriteLine("Welcome to the random number guessing game");
+                Console.WriteLine("Easy (1)");
+                Console.WriteLine("Medium (2)");
+                Console.WriteLine("Hard (3)");
+                do
+                {
+                    invalidInput = true;                  
+                    Console.Write("Enter the level you want to play: ");
+                    try {
+                        levelChoice = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch {
+                        levelChoice = 0;
+                    }
+                    switch (levelChoice) {
+                        case 1:
+                            numberRange = 10;
+                            break;
+                        case 2:
+                            numberRange = 25;
+                            break;
+                        case 3:
+                            numberRange = 50;
+                            break;
+                        default:
+                            invalidInput = false;
+                            Console.WriteLine("That is not an level option!");
+                            Console.WriteLine();
+                            break;
+                    }
+                } while (invalidInput == false);
+                Levels.Game(numberRange);
+                Console.Write("Enter 'a' to play again: ");
+                playAgainInput = Console.ReadLine();
+                if (playAgainInput.Trim() == "a") {
+                    continue;
                 }
-            catch {
-                levelChoice = 0;
+                else {
+                    break;
                 }
-            switch (levelChoice) {
-                    case 1:
-                        numberRange = 10;
-                        break;
-                    case 2:
-                        numberRange = 25;
-                        break;
-                    case 3:
-                        numberRange = 50;
-                        break;
-                    default:
-                        hasChosen = false;
-                        Console.WriteLine("That is not an level option!");
-                        Console.WriteLine();
-                        break;
-                }                                       
-            } while (hasChosen == false);
-            Console.ReadKey();
-            Levels.Game(numberRange);          
+            } while (true);
         }
     }
 }
--- Guessing game/levels.cs
+++ Guessing game/levels.cs
@@ -6,199 +6,44 @@ namespace Guessing_game
 {
     class Levels
     {
-        public static void easy()
-        {
-            Random random = new Random();
-            int randomNumber = random.Next(1, 11);
-
-            Console.WriteLine("Welcome to the random guessing game");
-            Console.Write("Guess a number between 1 and 10: ");
-
-            int guess = 0;
-            try
-            {
-                guess = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (System.FormatException)
-            {
-            }
-
-            while (guess != randomNumber)
-            {
-                try
-                {
-                    if(guess == 99)
-                    {
-                        Console.WriteLine("Congratulations, you found the cheat code! The awnser is: ");
-                        Console.WriteLine(randomNumber);
-                        Console.Write("Now type it in: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else if (guess <= 0 || guess >= 11)
-                    {
-                        Console.Write("That is not a valid guess! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-
-                    else if (guess > randomNumber)
-                    {
-                        Console.Write("That's too high! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else if (guess < randomNumber)
-                    {
-                        Console.Write("That's too low! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("Unknown Error");
-                    }
-                }
-                catch (System.FormatException)
-                {
-                    Console.WriteLine("That is not a number!");
-                }
-
-            }
-
-            if (guess == randomNumber)
-            {
-                Console.WriteLine("You win!");
-            }
-        }
-
         internal static void Game(int numberRange)
         {
-            throw new NotImplementedException();
-        }
-
-        public static void medium()
-        {
             Random random = new Random();
-            int randomNumber = random.Next(1, 26);
-
+            int randomNumber = random.Next(1, (numberRange + 1));
+            int guess;
+            Console.Clear();
+            Frame.Title();
             Console.WriteLine("Welcome to the random guessing game");
-            Console.Write("Guess a number between 1 and 25: ");
-
-            int guess = 0;
-            try
-            {
-                guess = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (System.FormatException)
-            {
-            }
-
-            while (guess != randomNumber)
-            {
-
-                try
-                {
-                    if (guess == 99)
-                    {
-                        Console.WriteLine("Congratulations, you found the cheat code! The awnser is: ");
-                        Console.WriteLine(randomNumber);
-                        Console.Write("Now type it in: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else if (guess <= 0 || guess >= 26)
-                    {
-                        Console.Write("That is not a valid guess! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-
-                    else if (guess > randomNumber)
-                    {
-                        Console.Write("That's too high! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else if (guess < randomNumber)
-                    {
-                        Console.Write("That's too low! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("Unknown Error");
-                    }
+            Console.Write("Guess a number between 1 and " + numberRange + ": ");
+            do {
+                try {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }                
+                catch {
+                    guess = 0;
                 }
-                catch (System.FormatException)
-                {
-                    Console.WriteLine("That is not a number!");
+                Console.WriteLine();
+                if (guess == 99) {
+                    Console.WriteLine("Congratulations, you found the cheat code! The awnser is: " + randomNumber);
+                    Console.Write("Now type it in: ");
                 }
-
-            }
-
-            if (guess == randomNumber)
-            {
-                Console.WriteLine("You win!");
-            }
-        }
-
-        public static void hard()
-        {
-            Random random = new Random();
-            int randomNumber = random.Next(1, 51);
-
-            Console.WriteLine("Welcome to the random guessing game");
-            Console.Write("Guess a number between 1 and 50: ");
-
-            int guess = 0;
-            try
-            {
-                guess = Convert.ToInt32(Console.ReadLine());
-            }
-            catch (System.FormatException)
-            {
-            }
-
-            while (guess != randomNumber)
-            {
-                try
-                {
-                    if (guess == 99)
-                    {
-                        Console.WriteLine("Congratulations, you found the cheat code! The awnser is: ");
-                        Console.WriteLine(randomNumber);
-                        Console.Write("Now type it in: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else if (guess <= 0 || guess >= 51)
-                    {
-                        Console.Write("That is not a valid guess! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-
-                    else if (guess > randomNumber)
-                    {
-                        Console.Write("That's too high! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else if (guess < randomNumber)
-                    {
-                        Console.Write("That's too low! Try again: ");
-                        guess = Convert.ToInt32(Console.ReadLine());
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("Unknown Error");
-                    }
+                else if (guess < 1 || guess > numberRange) {
+                    Console.Write("That is not a valid guess! Try again: ");
                 }
-                catch (System.FormatException)
-                {
-                    Console.WriteLine("That is not a number!");
+                else if (guess > randomNumber) {
+                    Console.Write("That's too high! Try again: ");
                 }
-
-            }
-
-            if (guess == randomNumber)
-            {
-                Console.WriteLine("You win!");
-            }
-        }
+                else if (guess < randomNumber) {
+                    Console.Write("That's too low! Try again: ");
+                }
+                else {
+                    Console.Write("That is not a valid guess! Try again: ");
+                }
+            } while (guess != randomNumber);
+            Console.Clear();
+            Frame.Title();
+            Console.WriteLine("You win!");
+            Console.WriteLine();
+        }        
     }
 }
